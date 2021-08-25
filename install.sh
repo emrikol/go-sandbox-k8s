91c478a2ca42f25edbd5ca794f9bb4befcc5459a
Derrick Tennant <derrick.tennant@automattic.com>
2021-08-25 14:14:00 -0400

    Add source to bashrc

--- install.sh
+++ install.sh
@@ -2,4 +2,9 @@
 # Clone or download GH repo
 git clone --recursive git@github.com:emrikol/go-sandbox-k8s.git ~/go-sandbox 2> /dev/null || git -C ~/go-sandbox pull
 
-source ~/go-sandbox/bash_profile
+# Add source to bashrc if not exists
+grep -c source ~/.bashrc &> /dev/null
+ret=$?
+if [ $ret -ne 0 ]; then
+	echo "source ~/go-sandbox/bash_profile" >> ~/.bashrc
+fi
