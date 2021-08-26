ad974f1c1f51963cd1948f247793044ff5406f45
Derrick Tennant <derrick.tennant@automattic.com>
2021-08-25 20:17:03 -0400

    Missed one bashrc

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 git clone --recursive git@github.com:emrikol/go-sandbox-k8s.git ~/go-sandbox 2> /dev/null || git -C ~/go-sandbox pull
 
 # Add source to bashrc if not exists
-grep -c source ~/.bashrc &> /dev/null
+grep -c source ~/.bash_profile &> /dev/null
 ret=$?
 if [ $ret -ne 0 ]; then
 	echo "source ~/go-sandbox/bash_profile" >> ~/.bash_profile
