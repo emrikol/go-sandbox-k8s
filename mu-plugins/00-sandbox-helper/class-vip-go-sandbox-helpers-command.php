8aef55b8e0c296979f5f73c579aeed59b91980f2
Derrick Tennant <emrikol@users.noreply.github.com>
2023-04-20 10:48:41 -0400

    Update class-vip-go-sandbox-helpers-command.php

--- mu-plugins/00-sandbox-helper/class-vip-go-sandbox-helpers-command.php
+++ mu-plugins/00-sandbox-helper/class-vip-go-sandbox-helpers-command.php
@@ -212,6 +212,185 @@ END;
 
 		WP_CLI::success( sprintf( 'Undeleted %s', $url ) );
 	}
+
+	/**
+	 * Gets size of database tables for the current site.
+	 *
+	 * ## OPTIONS
+	 *
+	 * [--subsite]
+	 * : Outputs the size of individual subsites as a whole in a multisite installation.
+	 *
+	 * [--raw]
+	 * : Outputs full size in bytes instead of human readable sizes.
+	 *
+	 * [--order_by=<Total>]
+	 * : Allows custom ordering of the data.
+	 * ---
+	 * default: Total
+	 * options:
+	 *   - Table
+	 *   - Data Size
+	 *   - Index Size
+	 *   - Total
+	 * ---
+	 *
+	 * [--order=<asc>]
+	 * : Allows custom ordering direction of the data.
+	 * ---
+	 * default: asc
+	 * options:
+	 *   - asc
+	 *   - desc
+	 * ---
+	 *
+	 * [--format=<format>]
+	 * : Render output in a particular format.
+	 * ---
+	 * default: table
+	 * options:
+	 *   - table
+	 *   - csv
+	 *   - json
+	 *   - count
+	 *   - yaml
+	 * ---
+	 *
+	 * @subcommand db-size
+	 */
+	public function db_size( $args, $assoc_args ) {
+		global $wpdb;
+
+		$output   = array();
+		$db_size  = array( 'data' => 0, 'index' => 0 );
+		$order_by = WP_CLI\Utils\get_flag_value( $assoc_args, 'order_by', 'Total' );
+		$order    = WP_CLI\Utils\get_flag_value( $assoc_args, 'order', 'asc' );
+		$format   = WP_CLI\Utils\get_flag_value( $assoc_args, 'format', 'table' );
+		$raw      = (bool) WP_CLI\Utils\get_flag_value( $assoc_args, 'raw', false );
+		$subsite  = (bool) WP_CLI\Utils\get_flag_value( $assoc_args, 'subsite', false );
+
+		// Fetch list of tables from database.
+		$tables = array_map(
+			function( $val ) {
+				return $val[0];
+			},
+			$wpdb->get_results( 'SHOW TABLES;', ARRAY_N ) // phpcs:ignore WordPress.DB.DirectDatabaseQuery
+		);
+
+		// Initialize an array to store subsite sizes.
+		$subsite_sizes = array();
+
+		// Fetch table information from database.
+		foreach ($tables as $table) {
+			$table_info = $wpdb->get_row( "SHOW TABLE STATUS LIKE '$table'" ); // phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared
+
+			// Keep a running total of sizes.
+			$db_size['data']  += $table_info->Data_length; // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+			$db_size['index'] += $table_info->Index_length; // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+
+			if ( true === $subsite ) {
+				// Extract subsite ID from table name.
+				preg_match('/^wp_(\d+)_/', $table_info->Name, $matches);
+				$subsite_prefix = isset($matches[1]) ? "wp_{$matches[1]}_" : 'wp_';
+
+				// Calculate total size for each subsite.
+				if (!isset($subsite_sizes[$subsite_prefix])) {
+					$subsite_sizes[$subsite_prefix] = array(
+						'Data Size'  => 0,
+						'Index Size' => 0,
+						'Total'      => 0
+					);
+				}
+				$subsite_sizes[$subsite_prefix]['Data Size']  += $table_info->Data_length;
+				$subsite_sizes[$subsite_prefix]['Index Size'] += $table_info->Index_length;
+				$subsite_sizes[$subsite_prefix]['Total']      += $table_info->Data_length + $table_info->Index_length;
+			} else {
+				// Format output for WP-CLI's format_items function.
+				$output[] = array(
+					'Table'      => $table_info->Name, // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+					'Data Size'  => $table_info->Data_length, // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+					'Index Size' => $table_info->Index_length, // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+					'Total'      => $table_info->Data_length + $table_info->Index_length, // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+				);
+			}
+		}
+
+		// Format output for WP-CLI's format_items function.
+		if ( true === $subsite ) {
+			foreach ($subsite_sizes as $prefix => $sizes) {
+				$output[] = array(
+					'Subsite Prefix' => $prefix,
+					'Data Size'      => $sizes['Data Size'],
+					'Index Size'     => $sizes['Index Size'],
+					'Total'          => $sizes['Total']
+				);
+			}
+		}
+
+		// Sort table data.
+		self::sort_table_by( $order_by, $output, $order );
+
+		if ( ! $raw ) {
+			// Make data human readable.
+			foreach ( array_keys( $output ) as $key ) {
+				$output[ $key ]['Data Size']  = size_format( $output[ $key ]['Data Size'] );
+				$output[ $key ]['Index Size'] = size_format( $output[ $key ]['Index Size'] );
+				$output[ $key ]['Total']      = size_format( $output[ $key ]['Total'] );
+			}
+		}
+
+		// Output data.
+		if ( true === $subsite ) {
+			WP_CLI\Utils\format_items( $format, $output, array( 'Subsite Prefix', 'Data Size', 'Index Size', 'Total' ) );
+		} else {
+			WP_CLI\Utils\format_items( $format, $output, array( 'Table', 'Data Size', 'Index Size', 'Total' ) );
+		}
+
+		// Output totals if we're not piping somewhere.
+		if ( ! WP_CLI\Utils\isPiped() ) {
+			WP_CLI::success(
+				sprintf(
+					'Total size of the database for %s is %s. Data: %s; Index: %s',
+					home_url(),
+					WP_CLI::colorize( '%g' . size_format( $db_size['data'] + $db_size['index'] ) . '%n' ),
+					WP_CLI::colorize( '%g' . size_format( $db_size['data'] ) . '%n' ),
+					WP_CLI::colorize( '%g' . size_format( $db_size['index'] ) . '%n' )
+				)
+			);
+		}
+	}
+
+	/**
+	 * Sorts a table by a specific field and direction.
+	 *
+	 * @param string $field The field to order by.
+	 * @param array  &$array The table array to sort.
+	 * @param string $direction The direction to sort. Ascending ('asc') or descending ('desc').
+	 */
+	private function sort_table_by( $field, &$array, $direction ) {
+		// Taken from https://joshtronic.com/2013/09/23/sorting-associative-array-specific-key/ Thanks!
+		usort(
+			$array,
+			function ( $a, $b ) use ( $field, $direction ) {
+				$a = $a[ $field ];
+				$b = $b[ $field ];
+
+				if ( $a === $b ) {
+					return 0;
+				}
+
+				switch ( $direction ) {
+					case 'asc':
+						return ( $a < $b ) ? -1 : 1;
+					case 'desc':
+						return ( $a > $b ) ? -1 : 1;
+					default:
+						return 0;
+				}
+			}
+		);
+		return true;
+	}
 }
 
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
